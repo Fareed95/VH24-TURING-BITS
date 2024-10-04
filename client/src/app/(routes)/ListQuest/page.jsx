8cf3d4fa6063fcc59d2f8127bbe06a151fb27608
Rehbar khan <rehbarkhan.2711@gmail.com>
2024-10-05 01:43:52 +0530

    Video Call half done 1

---
+++ client/src/app/(routes)/ListQuest/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Coatches } from '@/app/components/Coatches'
+function page() {
+  return (
+    <div>
+        <Coatches></Coatches>
+
+    </div>
+  )
+}
+
+export default page
