413e4a4f924f24b6292ca1c100cb6d700505c400
Rehbar khan <rehbarkhan.2711@gmail.com>
2024-10-04 18:02:51 +0530

    frontend test

--- client/src/app/(routes)/Signup-M/page.jsx
+++ client/src/app/(routes)/Signup-M/page.jsx
@@ -2,9 +2,10 @@
 import React from 'react'
 import {Signup} from '@/app/components/Signup'
 function page() {
+  const trainer = true; // Set the trainer value to false
   return (
     <div className='pt-[30vw] p-5 sm:pt-[10vw]'>
-      <Signup></Signup>
+      <Signup trainer={trainer}></Signup> {/* Pass the trainer value as a prop */}
     </div>
   )
 }
